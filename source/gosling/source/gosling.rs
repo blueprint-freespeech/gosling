34fa093f5f02045f5054f369eed49876f33569d9
Richard Pospesel <richard@torproject.org>
2021-08-24 17:47:17 +0200

    tor_crypto: stubbed out tor_crypto static lib project file

--- source/gosling/source/gosling.rs
+++ source/gosling/source/gosling.rs
@@ -1,5 +1,11 @@
-#[no_mangle]
-pub extern "C" fn rust_hello_world() {
-    println!("Hallo van Rust!");
+extern {
+    fn stub_func() -> i32;
 }
 
+#[no_mangle]
+pub extern "C" fn rust_hello_world() -> i32 {
+    println!("Hallo van Rust!");
+    unsafe {
+        return stub_func();
+    }
+}
---
+++ source/tor_crypto/tor_stubs.c
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#include "orconfig.h"
+
+int32_t stub_func()
+{
+    puts("Inside stub_func()");
+    return 42;
+}
