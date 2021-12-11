161f6438976abf55427e79655058199240a1b5df
Richard Pospesel <richard@torproject.org>
2021-12-11 16:02:16 +0100

    tor_crypto: added precompiled headers and stub functions

--- source/tor_crypto/tor_stubs.c
+++ source/tor_crypto/tor_stubs.c
@@ -1,10 +1,49 @@
-#include <stdint.h>
-#include <stdio.h>
-
-#include "orconfig.h"
 
 int32_t stub_func()
 {
     puts("Inside stub_func()");
     return 42;
+}
+
+void *tor_malloc_(size_t size)
+{
+    return malloc(size);
+}
+
+void *tor_malloc_zero_(size_t size)
+{
+    void* retval = tor_malloc_(size);
+    if (retval) {
+        memset(retval, 0x00, size);
+    }
+    return retval;
+}
+
+void tor_assertion_failed_(
+    const char *fname,
+    unsigned int line,
+    const char *func,
+    const char *expr,
+    const char *fmt,
+    ...)
+{
+    (void)fname;
+    (void)line;
+    (void)func;
+    (void)expr;
+    (void)fmt;
+}
+
+void tor_abort_(void)
+{
+    abort();
+}
+
+void log_fn_(int severity, log_domain_mask_t domain, const char *fn,
+    const char *format, ...)
+{
+    (void)severity;
+    (void)domain;
+    (void)fn;
+    (void)format;
 }
