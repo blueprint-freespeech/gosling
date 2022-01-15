1e9cc7efc6d98c9e34fd8e673cb59609b10c36b2
Richard Pospesel <richard@torproject.org>
2022-01-15 19:47:49 +0100

    libgosling: added std::default_delete implementation to c++ header

--- source/gosling/libgosling.hpp
+++ source/gosling/libgosling.hpp
@@ -2,6 +2,7 @@
 
 // c++
 #include <stdexcept>
+#include <memory>
 
 // gosling header
 #include <libgosling.h>
@@ -29,5 +30,32 @@ namespace gosling {
         }
     private:
         gosling_error* error_ = nullptr;
+    };
+}
+
+namespace std {
+    template<> class default_delete<gosling_ed25519_private_key> {
+    public:
+        void operator()(gosling_ed25519_private_key *val) {
+            gosling_ed25519_private_key_free(val);
+        }
+    };
+    template<> class default_delete<gosling_ed25519_public_key> {
+    public:
+        void operator()(gosling_ed25519_public_key *val) {
+            gosling_ed25519_public_key_free(val);
+        }
+    };
+    template<> class default_delete<gosling_ed25519_signature> {
+    public:
+        void operator()(gosling_ed25519_signature *val) {
+            gosling_ed25519_signature_free(val);
+        }
+    };
+    template<> class default_delete<gosling_v3_onion_service_id> {
+    public:
+        void operator()(gosling_v3_onion_service_id *val) {
+            gosling_v3_onion_service_id_free(val);
+        }
     };
 }
