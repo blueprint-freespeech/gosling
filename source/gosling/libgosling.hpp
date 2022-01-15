8e366f33e3054c3129d499fafefedb315fcf055c
Richard Pospesel <richard@torproject.org>
2022-01-15 20:11:14 +0100

    libgosling: added gosling::out function for passing unique_ptr as an out param

--- source/gosling/libgosling.hpp
+++ source/gosling/libgosling.hpp
@@ -31,6 +31,50 @@ namespace gosling {
     private:
         gosling_error* error_ = nullptr;
     };
+
+    //
+    // helper class for populating out T** params into unique_ptr<T> objects
+    //
+    template<typename T>
+    class out_unique_ptr
+    {
+    public:
+        out_unique_ptr() = delete;
+        out_unique_ptr(const out_unique_ptr&) = delete;
+        out_unique_ptr(out_unique_ptr&&) = delete;
+        out_unique_ptr& operator=(const out_unique_ptr&) = delete;
+        out_unique_ptr& operator=(out_unique_ptr&&) = delete;
+
+        out_unique_ptr(std::unique_ptr<T>& u) : u_(u) {}
+        ~out_unique_ptr()
+        {
+            u_.reset(t_);
+        }
+
+        operator T**()
+        {
+            return &t_;
+        }
+
+    private:
+        T* t_ = nullptr;
+        std::unique_ptr<T>& u_;
+    };
+
+    //
+    // helper function for populating out T** params
+    // example:
+    //
+    // void give_int(int** outInt);
+    // std::unique_ptr<int> pint;
+    // give_int(tego::out(pint));
+    // int val = *pint;
+    //
+    template<typename T>
+    out_unique_ptr<T> out(std::unique_ptr<T>& ptr)
+    {
+        return {ptr};
+    }
 }
 
 namespace std {
