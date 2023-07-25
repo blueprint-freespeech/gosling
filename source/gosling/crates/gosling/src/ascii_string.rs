758db367e531bfebee2ed2a2605479b2f4b73f99
Richard Pospesel <richard@torproject.org>
2023-07-25 02:21:38 +0000

    gosling: added test for AsciiString

--- source/gosling/crates/gosling/src/ascii_string.rs
+++ source/gosling/crates/gosling/src/ascii_string.rs
@@ -1,3 +1,5 @@
+#[cfg(test)]
+use anyhow::bail;
 use std::ops::Deref;
 
 #[derive(thiserror::Error, Debug)]
@@ -40,4 +42,36 @@ impl std::fmt::Display for AsciiString {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.value.fmt(f)
     }
+}
+
+#[test]
+fn test_ascii_string() -> anyhow::Result<()> {
+    let valid_ascii: [String; 8] = [
+        "".to_string(),
+        " !\"#$%&'()*+,-./".to_string(),
+        "0123456789".to_string(),
+        ":<=>?@".to_string(),
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ".to_string(),
+        "[\\]^_`".to_string(),
+        "abcdefghijklmnopqstuvwxyz".to_string(),
+        "{|}~".to_string(),
+    ];
+
+    for string in valid_ascii {
+        match AsciiString::new(string) {
+            Ok(string) => println!("ascii: '{}'", string),
+            Err(err) => bail!("unexpected error: {}", err),
+        }
+    }
+
+    let utf8: [String; 2] = ["❤".to_string(), "heart ❤".to_string()];
+
+    for string in utf8 {
+        match AsciiString::new(string) {
+            Ok(string) => bail!("this is not ascii: {}", string),
+            Err(_) => (),
+        }
+    }
+
+    Ok(())
 }
