bae845128e00833ddf4122a774271ba64f3852b0
pospeselr <pospeselr@users.noreply.github.com>
2023-09-25 15:10:30 +0000

    deploy: 94e3c73e6e9029772649a874c99c32047bc53ced

--- crates/honk_rpc/honk_rpc/sidebar-items.js
+++ crates/honk_rpc/honk_rpc/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["DEFAULT_MAX_MESSAGE_SIZE","DEFAULT_MAX_WAIT_TIME"],"enum":["Error","ErrorCode","Response"],"struct":["Session"],"trait":["ApiSet"],"type":["RequestCookie"]};
+window.SIDEBAR_ITEMS = {"constant":["DEFAULT_MAX_MESSAGE_SIZE","DEFAULT_MAX_WAIT_TIME"],"enum":["Error","ErrorCode","Response"],"fn":["get_error_section_size","get_message_overhead","get_request_section_size","get_response_section_size"],"struct":["Session"],"trait":["ApiSet"],"type":["RequestCookie"]};
