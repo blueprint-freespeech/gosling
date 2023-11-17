79db158fd1141e2a073f63819a01f95c1290de84
pospeselr <pospeselr@users.noreply.github.com>
2023-11-17 17:14:25 +0000

    deploy: e2e8706ea5557527b2573f07e0c7524040cacf6d

--- crates/implementors/core/default/trait.Default.js
+++ crates/implementors/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"tor_interface":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"tor_interface/mock_tor_client/struct.MockTorClient.html\" title=\"struct tor_interface::mock_tor_client::MockTorClient\">MockTorClient</a>"]]
+"tor_interface":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"tor_interface/mock_tor_client/struct.MockTorClient.html\" title=\"struct tor_interface::mock_tor_client::MockTorClient\">MockTorClient</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
