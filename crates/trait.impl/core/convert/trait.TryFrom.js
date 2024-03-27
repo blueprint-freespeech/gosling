6c9ebf5597ad5f91e651aa7ef75d5d7591e4ed1d
pospeselr <pospeselr@users.noreply.github.com>
2024-03-27 04:35:57 +0000

    deploy: 88aab88d8bc0c89960ea3b1c65dceb8f2f57c1c5

--- crates/trait.impl/core/convert/trait.TryFrom.js
+++ crates/trait.impl/core/convert/trait.TryFrom.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"gosling":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.76.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.76.0/std/primitive.i32.html\">i32</a>&gt; for <a class=\"enum\" href=\"gosling/gosling/enum.RpcError.html\" title=\"enum gosling::gosling::RpcError\">RpcError</a>"]]
+"gosling":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.77.0/std/primitive.i32.html\">i32</a>&gt; for <a class=\"enum\" href=\"gosling/gosling/enum.RpcError.html\" title=\"enum gosling::gosling::RpcError\">RpcError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
