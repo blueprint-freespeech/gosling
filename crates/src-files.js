87f81b6540ab2209454f65df28e5a8479c1386f1
pospeselr <pospeselr@users.noreply.github.com>
2024-04-02 02:56:15 +0000

    deploy: b45c71270ee4672daf3f0b3ae87542dbd2cf7a5d

--- crates/src-files.js
+++ crates/src-files.js
@@ -2,6 +2,6 @@ var srcIndex = new Map(JSON.parse('[\
 ["cgosling_proc_macros",["",[],["lib.rs"]]],\
 ["gosling",["",[],["ascii_string.rs","context.rs","endpoint_client.rs","endpoint_server.rs","gosling.rs","identity_client.rs","identity_server.rs","lib.rs"]]],\
 ["honk_rpc",["",[],["byte_counter.rs","honk_rpc.rs","lib.rs"]]],\
-["tor_interface",["",[],["legacy_tor_client.rs","legacy_tor_control_stream.rs","legacy_tor_controller.rs","legacy_tor_process.rs","legacy_tor_version.rs","lib.rs","mock_tor_client.rs","tor_crypto.rs","tor_provider.rs"]]]\
+["tor_interface",["",[],["lib.rs","tor_crypto.rs","tor_provider.rs"]]]\
 ]'));
 createSrcSidebar();
