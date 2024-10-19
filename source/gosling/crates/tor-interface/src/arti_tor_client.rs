acaaa9975672f21c5a13a6c3af4e7bd361c491d1
Morgan <morgan@torproject.org>
2024-10-19 23:10:04 +0000

    tor-interface: implement stub ArtiTorClient TorProvider

---
+++ source/gosling/crates/tor-interface/src/arti_tor_client.rs
@@ -0,0 +1,85 @@
+// internal crates
+use crate::tor_crypto::*;
+use crate::tor_provider;
+use crate::tor_provider::*;
+
+
+/// [`ArtiTorClient`]-specific error type
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("not implemented")]
+    NotImplemented(),
+}
+
+impl From<Error> for crate::tor_provider::Error {
+    fn from(error: Error) -> Self {
+        crate::tor_provider::Error::Generic(error.to_string())
+    }
+}
+
+#[derive(Clone, Debug)]
+pub enum ArtiTorClientConfig {
+    Bundled {
+    },
+    System {
+
+    },
+}
+
+pub struct ArtiTorClient {
+
+}
+
+impl ArtiTorClient {
+    pub fn new(config: ArtiTorClientConfig) -> Result<Self, tor_provider::Error> {
+        Err(Error::NotImplemented().into())
+    }
+}
+
+impl TorProvider for ArtiTorClient {
+    fn update(&mut self) -> Result<Vec<TorEvent>, tor_provider::Error> {
+        Err(Error::NotImplemented().into())
+    }
+
+    fn bootstrap(&mut self) -> Result<(), tor_provider::Error> {
+        Err(Error::NotImplemented().into())
+    }
+
+    fn add_client_auth(
+        &mut self,
+        _service_id: &V3OnionServiceId,
+        _client_auth: &X25519PrivateKey,
+    ) -> Result<(), tor_provider::Error> {
+        Err(Error::NotImplemented().into())
+    }
+
+    fn remove_client_auth(
+        &mut self,
+        _service_id: &V3OnionServiceId,
+    ) -> Result<(), tor_provider::Error> {
+        Err(Error::NotImplemented().into())
+    }
+
+    fn connect(
+        &mut self,
+        _target: TargetAddr,
+        _circuit: Option<CircuitToken>,
+    ) -> Result<OnionStream, tor_provider::Error> {
+        Err(Error::NotImplemented().into())
+    }
+
+    fn listener(
+        &mut self,
+        _private_key: &Ed25519PrivateKey,
+        _virt_port: u16,
+        _authorized_clients: Option<&[X25519PublicKey]>,
+    ) -> Result<OnionListener, tor_provider::Error> {
+        Err(Error::NotImplemented().into())
+    }
+
+    fn generate_token(&mut self) -> CircuitToken {
+        0usize
+    }
+
+    fn release_token(&mut self, _token: CircuitToken) {}
+}
