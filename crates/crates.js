84aeb5c7a6b3b33d1d43d4744978112c2dabf319
morganava <morganava@users.noreply.github.com>
2024-10-27 22:33:21 +0000

    deploy: 1df8d909578ea820d58783e7f75828546ffd5b82

--- crates/crates.js
+++ crates/crates.js
@@ -1,2 +1,1 @@
 window.ALL_CRATES = ["gosling","honk_rpc","tor_interface"];
-//{"start":21,"fragment_lengths":[9,11,16]}
--- crates/trait.impl/std/io/trait.Write.js
+++ crates/trait.impl/std/io/trait.Write.js
@@ -1,9 +1,3 @@
-(function() {
-    var implementors = Object.fromEntries([["tor_interface",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"tor_interface/tor_provider/struct.OnionStream.html\" title=\"struct tor_interface::tor_provider::OnionStream\">OnionStream</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[312]}
+(function() {var implementors = {
+"tor_interface":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a> for <a class=\"struct\" href=\"tor_interface/tor_provider/struct.OnionStream.html\" title=\"struct tor_interface::tor_provider::OnionStream\">OnionStream</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
