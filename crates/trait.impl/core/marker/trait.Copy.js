b0bd1e27b22710cd44d5c838a0d9a4f0a8b99909
morganava <morganava@users.noreply.github.com>
2024-11-06 20:47:44 +0000

    deploy: e996c0740cdada8e78617fc0a29427e74c19e9af

--- crates/trait.impl/core/marker/trait.Copy.js
+++ crates/trait.impl/core/marker/trait.Copy.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"tor_interface":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"tor_interface/tor_crypto/enum.SignBit.html\" title=\"enum tor_interface::tor_crypto::SignBit\">SignBit</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["tor_interface",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"tor_interface/tor_crypto/enum.SignBit.html\" title=\"enum tor_interface::tor_crypto::SignBit\">SignBit</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[297]}
--- crates/trait.impl/std/io/trait.Read.js
+++ crates/trait.impl/std/io/trait.Read.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"tor_interface":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.81.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"tor_interface/tor_provider/struct.OnionStream.html\" title=\"struct tor_interface::tor_provider::OnionStream\">OnionStream</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["tor_interface",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"tor_interface/tor_provider/struct.OnionStream.html\" title=\"struct tor_interface::tor_provider::OnionStream\">OnionStream</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[309]}
