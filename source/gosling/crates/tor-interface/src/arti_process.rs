e95707fa3fc2858d6a9bac7b2db75d68c1dd2f37
Morgan <morgan@torproject.org>
2025-02-08 15:34:36 +0000

    tor-interface: updated arti to 0.27.0

--- source/gosling/crates/tor-interface/src/arti_process.rs
+++ source/gosling/crates/tor-interface/src/arti_process.rs
@@ -1,19 +1,16 @@
 // standard
 use std::fs;
+#[cfg(unix)]
+use std::os::unix::fs::PermissionsExt;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
 use std::ops::Drop;
-use std::process;
 use std::process::{Child, ChildStdout, Command, Stdio};
 use std::path::Path;
 use std::sync::{Mutex, Weak};
-use std::time::{Duration, Instant};
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("not implemented")]
-    NotImplemented(),
-
     #[error("provided arti bin path '{0}' must be an absolute path")]
     ArtiBinPathNotAbsolute(String),
 
@@ -26,11 +23,20 @@ pub enum Error {
     #[error("file exists in provided data directory path '{0}'")]
     ArtiDataDirectoryPathExistsAsFile(String),
 
+    #[error("unable to set permissions for data directory: {0}")]
+    ArtiDataDirectorySetPermissionsFailed(#[source] std::io::Error),
+
     #[error("failed to create arti.toml file: {0}")]
     ArtiTomlFileCreationFailed(#[source] std::io::Error),
 
     #[error("failed to write arti.toml file: {0}")]
     ArtiTomlFileWriteFailed(#[source] std::io::Error),
+
+    #[error("failed to create rpc.toml file: {0}")]
+    RpcTomlFileCreationFailed(#[source] std::io::Error),
+
+    #[error("failed to write rpc.toml file: {0}")]
+    RpcTomlFileWriteFailed(#[source] std::io::Error),
 
     #[error("failed to start arti process: {0}")]
     ArtiProcessStartFailed(#[source] std::io::Error),
@@ -73,27 +79,88 @@ impl ArtiProcess {
             )));
         }
 
-        // construct paths to arti files file
-        let arti_toml = data_directory.join("arti.toml");
+        // arti data directory must not be world-writable on unix platforms when using a unix domain socket endpoint
+        if cfg!(unix) {
+            let perms = PermissionsExt::from_mode(0o700);
+            fs::set_permissions(data_directory, perms).map_err(Error::ArtiDataDirectorySetPermissionsFailed)?;
+        }
 
-        // write arti.toml settings file (always overwrite)
+        // construct paths to arti files file
+        let arti_toml = data_directory.join("arti.toml").display().to_string();
         let cache_dir = data_directory.join("cache").display().to_string();
         let state_dir = data_directory.join("state").display().to_string();
-        let rpc_listen = data_directory.join("SOCKET").display().to_string();
 
-        let arti_toml_content = format!("\
+        let mut arti_toml_content = format!("\
+        [rpc]\n\
+        enable = true\n\n\
+        [rpc.listen.user-default]\n\
+        enable = false\n\n\
+        [rpc.listen.system-default]\n\
+        enable = false\n\n\
         [storage]\n\
         cache_dir = \"{cache_dir}\"\n\
-        state_dir = \"{state_dir}\"\n\
+        state_dir = \"{state_dir}\"\n\n\
         [storage.keystore]\n\
-        enabled = true\n\
+        enabled = true\n\n\
         [storage.keystore.primary]\n\
-        kind = \"ephemeral\"\n\
+        kind = \"ephemeral\"\n\n\
         [storage.permissions]\n\
-        dangerously_trust_everyone = true\n\
-        [rpc]\n\
-        rpc_listen = \"{rpc_listen}\"\n
+        dangerously_trust_everyone = true\n\n\
         ");
+
+        let connect_string = if cfg!(unix) {
+            // use domain socket for unix
+            let unix_rpc_toml_path = data_directory.join("rpc.toml").display().to_string();
+
+            arti_toml_content.push_str(format!("\
+            [rpc.listen.unix-point]\n\
+            enable = true\n\
+            file = \"{unix_rpc_toml_path}\"\n\n\
+            ").as_str());
+
+            let socket_path = data_directory.join("rpc.socket").display().to_string();
+
+            let unix_rpc_toml_content = format!("\
+            [connect]\n\
+            socket = \"unix:{socket_path}\"\n\
+            auth = \"none\"\n\
+            ");
+
+            let mut unix_rpc_toml_file =
+                File::create(&unix_rpc_toml_path).map_err(Error::RpcTomlFileCreationFailed)?;
+            unix_rpc_toml_file
+                .write_all(unix_rpc_toml_content.as_bytes())
+                .map_err(Error::RpcTomlFileWriteFailed)?;
+
+            unix_rpc_toml_path
+        } else {
+            // use tcp socket everywhere else
+            let tcp_rpc_toml_path = data_directory.join("rpc.toml").display().to_string();
+
+            arti_toml_content.push_str(format!("\
+            [rpc.listen.tcp-point]\n\
+            enable = true\n\
+            file = \"{tcp_rpc_toml_path}\"\n\n\
+            ").as_str());
+
+            let cookie_path = data_directory.join("rpc.cookie").display().to_string();
+
+            const RPC_PORT: u16 = 18929;
+
+            let tcp_rpc_toml_content = format!("\
+            [connect]\n\
+            socket = \"inet:127.0.0.1:{RPC_PORT}\"\n\
+            auth = {{ cookie = {{ path = \"{cookie_path}\" }} }}\n\
+            ");
+
+            let mut tcp_rpc_toml_file =
+                File::create(&tcp_rpc_toml_path).map_err(Error::RpcTomlFileCreationFailed)?;
+            tcp_rpc_toml_file
+                .write_all(tcp_rpc_toml_content.as_bytes())
+                .map_err(Error::RpcTomlFileWriteFailed)?;
+
+            tcp_rpc_toml_path
+        };
 
         let mut arti_toml_file =
             File::create(&arti_toml).map_err(Error::ArtiTomlFileCreationFailed)?;
@@ -126,8 +193,6 @@ impl ArtiProcess {
                 ArtiProcess::read_stdout_task(&stdout_lines, stdout);
             })
             .map_err(Error::ArtiStdoutReadThreadSpawnFailed)?;
-
-        let connect_string = format!("unix:{rpc_listen}");
 
         Ok(ArtiProcess { process, connect_string })
     }
