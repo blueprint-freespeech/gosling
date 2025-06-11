57e0cc4d8b882c604b8bf6f87c3a5d851e509bfe
morganava <morganava@users.noreply.github.com>
2025-06-11 22:37:30 +0000

    deploy: 52d8a5b289cf9655f4601391dfe17e19023bd39e

--- crates/trait.impl/alloc/string/trait.ToString.js
+++ crates/trait.impl/alloc/string/trait.ToString.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["tor_interface",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"tor_interface/legacy_tor_version/struct.LegacyTorVersion.html\" title=\"struct tor_interface::legacy_tor_version::LegacyTorVersion\">LegacyTorVersion</a>"]]]]);
+    var implementors = Object.fromEntries([["tor_interface",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"tor_interface/legacy_tor_version/struct.LegacyTorVersion.html\" title=\"struct tor_interface::legacy_tor_version::LegacyTorVersion\">LegacyTorVersion</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- crates/trait.impl/core/ops/deref/trait.DerefMut.js
+++ crates/trait.impl/core/ops/deref/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["tor_interface",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"tor_interface/tor_provider/struct.OnionStream.html\" title=\"struct tor_interface::tor_provider::OnionStream\">OnionStream</a>"]]]]);
+    var implementors = Object.fromEntries([["tor_interface",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"tor_interface/tor_provider/struct.OnionStream.html\" title=\"struct tor_interface::tor_provider::OnionStream\">OnionStream</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
