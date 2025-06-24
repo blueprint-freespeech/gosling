55c4a898d8a5f62aac28fee882aa1f8bc51398e8
наб <nabijaczleweli@nabijaczleweli.xyz>
2025-06-24 15:58:22 +0200

    tor-interface: flatten redundant double-init

--- source/gosling/crates/tor-interface/src/legacy_tor_client.rs
+++ source/gosling/crates/tor-interface/src/legacy_tor_client.rs
@@ -651,13 +651,11 @@ impl TorProvider for LegacyTorClient {
             .local_addr()
             .map_err(Error::TcpListenerLocalAddrFailed)?;
 
-        let mut flags = AddOnionFlags {
+        let flags = AddOnionFlags {
             discard_pk: true,
+            v3_auth: authorized_clients.is_some(),
             ..Default::default()
         };
-        if authorized_clients.is_some() {
-            flags.v3_auth = true;
-        }
 
         let onion_addr = OnionAddr::V3(OnionAddrV3::new(
             V3OnionServiceId::from_private_key(private_key),
