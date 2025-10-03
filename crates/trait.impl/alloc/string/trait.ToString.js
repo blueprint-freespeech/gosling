f1c7fe36a757e716e742bedaad5b60cf1caffd09
morganava <morganava@users.noreply.github.com>
2025-10-03 13:30:12 +0000

    deploy: aff524511ced4bc7b6aa6268a23e6ddbf4c16931

--- crates/trait.impl/alloc/string/trait.ToString.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["tor_interface",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"tor_interface/legacy_tor_version/struct.LegacyTorVersion.html\" title=\"struct tor_interface::legacy_tor_version::LegacyTorVersion\">LegacyTorVersion</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[360]}
